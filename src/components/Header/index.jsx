37012efbf3d4028035c89f5a59e4529f4f9f2748
Ismael Rodino <ismael.rodino@hotmail.com>
2021-09-19 13:59:25 -0300

    make SwitchButton work in dark mode

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,22 +3,27 @@ import { useContext } from 'react';
 import { FaLaptopCode } from 'react-icons/fa';
 import { SectionBackground } from '../SectionBackground';
 import { SwitchButton } from '../SwitchButton';
-import { useTheme, useThemeUpdate } from '../../providers/ThemeContext';
+import {
+  ThemeProvider,
+  useTheme,
+  useThemeUpdate,
+} from '../../providers/ThemeContext';
 
-export function Header({ children }) {
+export function Header({ toggled }) {
   const darkTheme = useTheme();
   const toggleTheme = useThemeUpdate();
-  console.log(darkTheme);
-
+  console.log(toggleTheme);
   return (
     <div className={darkTheme ? 'dark' : ''}>
       <SectionBackground background={true}>
-        <div className="h-screen flex items-center justify-center">
-          <div className="">
-            <FaLaptopCode />
-            <h1>Ismael</h1>
-            <p>dev</p>
-            <button onClick={toggleTheme}>tema dark</button>
+        <div className="flex items-center justify-center h-screen text-center">
+          <div>
+            <div className="flex justify-center">
+              <FaLaptopCode size={130} />
+            </div>
+            <h1 className="text-4xl">Ismael Rodino</h1>
+            <p className="text-3xl">Front-end Dev</p>
+            <SwitchButton />
           </div>
         </div>
       </SectionBackground>
--- src/components/SwitchButton/index.jsx
+++ src/components/SwitchButton/index.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import './styles.css';
 import { BsSun, BsMoon } from 'react-icons/bs';
+import { useTheme, useThemeUpdate } from '../../providers/ThemeContext';
+import { Header } from '../Header';
 
 export function SwitchButton() {
-  const [toggled, setToggled] = useState(false);
+  const toggleTheme = useThemeUpdate();
+  const darkTheme = useTheme();
+  console.log(darkTheme);
   return (
     <>
-      <label className="relative inline-block h-10 w-20 ">
+      <label className="relative inline-block w-20 h-10 ">
         <input
-          className="opacity-0 w-0 h-0"
+          className="w-0 h-0 opacity-0"
           type="checkbox"
-          onChange={(event) => setToggled(event.target.checked)}
+          onChange={toggleTheme}
         />
-        <span className="slider absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-gray-500 rounded-full flex items-center justify-center">
+        <span className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-gray-500 rounded-full cursor-pointer slider">
           <div className="flex">
-            <div className="flex text-yellow-500 pr-3">
+            <div className="flex pr-3 text-yellow-500">
               <BsSun size={26} />
             </div>
             <div className="text-yellow-200">
@@ -23,7 +27,6 @@ export function SwitchButton() {
           </div>
         </span>
       </label>
-      <p>{toggled ? 'on' : 'off'}</p>
     </>
   );
 }
